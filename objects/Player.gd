8ec208bf794b955c536ade6c6641ba31799b8669
Emir Cengiz <emircengiz21@gmail.com>
2022-02-12 12:12:43 +0300

    sex

--- objects/Player.gd
+++ objects/Player.gd
@@ -5,7 +5,7 @@ onready var camera = $Camera
 onready var anim = $AnimationPlayer
 
 var gravity =  Vector3.ZERO  # strength of gravity
-var speed = 4  # movement speed
+var speed = 3  # movement speed
 var jump_speed = 6  # jump strength
 var spin = 0.1  # rotation speed
 
@@ -15,6 +15,9 @@ var jump = false
 enum STATE {IDLE, WALKING}
 var current_state = STATE.IDLE
 
+var camera_pos = Vector3(0, 0.3, 5)
+var camera_rot = Vector3(-70, 0, 0)
+var camera_scal = Vector3(0.125, 0.446, 0.904)
 
 func get_input():
 	velocity.x = 0
@@ -37,11 +40,17 @@ func get_input():
 	match current_state:
 		STATE.IDLE:
 			anim.play("idle")
-		
 		STATE.WALKING:
 			anim.play("walk")
 
 func _physics_process(delta):
 	velocity += gravity * delta
 	get_input()
+	camera.translation.x = player.translation.x + camera_pos.x
+	camera.translation.y = player.translation.y + camera_pos.y
+	camera.translation.z = player.translation.z + camera_pos.z
+
+	camera.scale = camera_scal
+	camera.rotation = camera_rot
+
 	velocity = player.move_and_slide(velocity, Vector3.UP)
